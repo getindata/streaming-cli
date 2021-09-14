01af21d73836223886d0f387d19b117edb1c1de8
kalondar <marcin.kacperek@acast.com>
2021-09-14 11:27:45 +0200

    CLI mock

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="streamingcli",
-    version="0.1.0",
+    version="0.1.1",
     author="GetInData",
     author_email="office@getindata.com",
     description="Streaming platform CLI",
