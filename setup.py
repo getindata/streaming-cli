a86539b5549acd15743f940bc6f899735a604ace
kalondar <marcin.kacperek@acast.com>
2021-09-15 15:41:18 +0200

    version option works

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="streamingcli",
-    version="0.1.2",
+    version="0.1.3",
     author="GetInData",
     author_email="office@getindata.com",
     description="Streaming platform CLI",
--- streamingcli/__init__.py
+++ streamingcli/__init__.py
@@ -1,4 +1,3 @@
 from .main import cli
 
 __all__ = ["main"]
-__version__ = "0.1.3"
