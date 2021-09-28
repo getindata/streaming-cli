9d7eb37f7ed9a7b11bfe40e4b1baeb8ba343e598
kalondar <marcin.kacperek@acast.com>
2021-09-28 14:37:27 +0200

    Fix CLI command setup

--- setup.py
+++ setup.py
@@ -3,27 +3,25 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read()
+
 setuptools.setup(
     name="streamingcli",
-    version="0.1.3",
+    version="0.1.4",
     author="GetInData",
     author_email="office@getindata.com",
     description="Streaming platform CLI",
     long_description=long_description,
     url="https://gitlab.com/getindata/streaming-labs/streaming-cli",
     packages=setuptools.find_packages(),
+    python_requires='>=3.7',
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
         "Operating System :: OS Independent",
     ],
-    install_requires=[
-        'click',
-        'gitpython',
-        'pyyaml',
-        'kubernetes',
-        'dataclasses_json'
-    ],
-    py_modules=['scli'],
+    install_requires=[requirements],
+    py_modules=['streamingcli'],
     entry_points={
         'console_scripts': [
             'scli = streamingcli.main:cli',
