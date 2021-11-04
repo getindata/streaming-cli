8759cb1d0bbbafa12f570d2c4a7b650ef99a813a
kalondar <marcin.kacperek@acast.com>
2021-11-04 11:43:55 +0100

    Refactor test package structure

---
+++ tests/streamingcli/project/test_yaml_merger.py
@@ -0,0 +1,13 @@
+import pytest
+import hiyapyco
+
+from streamingcli.project.yaml_merger import YamlMerger
+
+
+class TestYamlMerger:
+    """Test merging two YAML files"""
+    def test_merge_two_yaml_files(self):
+        merged = YamlMerger.merge_two_yaml("tests/streamingcli/project/yaml/input_1.yml", "tests/streamingcli/project/yaml/input_2.yml")
+        expected = hiyapyco.dump(hiyapyco.load("tests/streamingcli/project/yaml/expected.yml"))
+
+        assert merged == expected
