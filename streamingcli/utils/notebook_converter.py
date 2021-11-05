5cfdb1d35aea7642325323cb70e3a46c3b8a5a4c
kalondar <marcin.kacperek@acast.com>
2021-11-05 14:21:39 +0100

    Refactor to static methods

--- streamingcli/utils/notebook_converter.py
+++ streamingcli/utils/notebook_converter.py
@@ -9,28 +9,33 @@ from streamingcli.project.template_loader import TemplateLoader
 
 class NotebookConverter:
 
-    def convert_notebook(self, notebook_path: str) -> str:
+    @staticmethod
+    def convert_notebook(notebook_path: str) -> str:
         try:
-            notebook = self.load_notebook(notebook_path=notebook_path)
-            sql_queries = self.get_sql_queries(notebook=notebook)
-            return self.render_flink_app(sql_queries=sql_queries)
+            notebook = NotebookConverter.load_notebook(notebook_path=notebook_path)
+            sql_queries = NotebookConverter.get_sql_queries(notebook=notebook)
+            return NotebookConverter.render_flink_app(sql_queries=sql_queries)
         except IOError:
             raise click.ClickException(f"Could not open file: {notebook_path}")
         except:
             raise click.ClickException(f"Unexpected exception: {sys.exc_info()}")
 
-    def load_notebook(self, notebook_path: str) -> nbformat.NotebookNode:
+    @staticmethod
+    def load_notebook(notebook_path: str) -> nbformat.NotebookNode:
         with open(notebook_path, 'r+') as notebook_file:
             return nbformat.reads(notebook_file.read(), as_version=4)
 
-    def filter_code_cells(self, notebook: nbformat.NotebookNode):
+    @staticmethod
+    def filter_code_cells(notebook: nbformat.NotebookNode):
         return filter(lambda cell: cell.cell_type == 'code' and cell.source.split('\n')[0] in JUPYTER_SQL_TAGS, notebook.cells)
 
-    def get_sql_queries(self, notebook: nbformat.NotebookNode):
-        code_cells = self.filter_code_cells(notebook=notebook)
+    @staticmethod
+    def get_sql_queries(notebook: nbformat.NotebookNode):
+        code_cells = NotebookConverter.filter_code_cells(notebook=notebook)
         return map(lambda cell: '\n'.join(cell.source.split('\n')[1:]) , code_cells)
 
-    def render_flink_app(self, sql_queries) -> str:
+    @staticmethod
+    def render_flink_app(sql_queries) -> str:
         flink_app_template = TemplateLoader.load_project_template("flink_app.py.template")
         flink_app_script = Environment().from_string(flink_app_template).render(
             sqls=sql_queries
